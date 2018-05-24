a382d0ac807cae858e2bd0388213b259f056e08d
tomgdow <thomasgdowling@gmail.com>
2018-05-24 22:08:24 +0100

    remove src file and update gulpfile

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let b = document.getElementById('buttons');
+let a = document.getElementById('async-buttons');
+
+b.firstChild.nextSibling.childNodes[3].checked=true;
+a.children[0].childNodes[5].firstElementChild.checked=true;
+
