c3518b66dcba7b1c35e254fc366e87ef7971054d
tomgdow <thomasgdowling@gmail.com>
2018-05-24 22:30:38 +0100

    Update 2018-05-24T21:30:35.119Z

--- src/js/nav.js
+++
@@ -1,8 +0,0 @@
-$(document).foundation(); 
-let gravatarOne = "https://s.gravatar.com/avatar/1670879794aed7c23742e5b1f759be94?s=40"
-let gravatarTwo = "https://s.gravatar.com/avatar/e901176d642c3782dd81a12d2dc83c75?s=40"
-
-document.getElementById('gravatarSmallView').src = gravatarOne;
-document.getElementById('gravatarMediumView').src = gravatarOne; 
-document.getElementById('gravatarTwoSmallView').src = gravatarTwo; 
-document.getElementById('gravatarTwoMediumView').src = gravatarTwo; 
